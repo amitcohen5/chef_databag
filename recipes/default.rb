70f7d06cf2aadff2fd739b0eac9b444b60054829
root <root@amitcohen51.mylabserver.com>
2018-02-03 12:30:15 +0000

    Dynamic creation of users and groups

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,14 +4,35 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+users = []
+
 search("users", "platform:centos").each do |user_data|
-  user user_data['id'] do
-    comment user_data['comment']
+  
+  user user_data['name'] do
     uid user_data['uid']
-    gid user_data['uid']
     home user_data['home']
     shell user_data['shell']
   end
-end 
+  users.push(user_data['name'])
+end
 
+#Create the Group if no exist
+
+search("users", "gid:1990").each do |group_data|
+  group "group_1" do
+    gid group_data['gid']
+    members users
+    not_if "getent group | grep group_1"
+    action :create
+  end
+end
+
+search("users", "gid:1990").each do |group_data|
+  group "group_1" do
+    gid group_data['gid']
+    members users
+    only_if "getent group | grep group_1"
+    action :modify
+  end
+end
 
