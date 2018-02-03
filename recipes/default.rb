8fe4d21c5942b9cb050464f68fb57a2d0fcf4128
amitcohen5 <amithezroni@gmail.com>
2018-02-03 09:59:18 +0000

    users with for loop

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+search("users", "platform:centos").each do |user_data|
+  user user_data['id'] do
+    comment user_data['comment']
+    uid user_data['uid']
+    gid user_data['uid']
+    home user_data['home']
+    shell user_data['shell']
+  end
+end 
+
+
